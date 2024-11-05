26dcdc454ed489577e5b15da69b87afd0fff3e7b
Gulirovisk <lucinhogarciacaboverde@gmail.com>
2024-11-05 20:25:56 -0300

    hello

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Uf(models.Model): # generos com seus livros, uf com suas cidades, emprestimos com seus livros, autores com seus livros
     sigla= models.CharField(max_length=50)
 
@@ -19,7 +18,6 @@ class Cidade(models.Model):
         verbose_name_plural = 'Cidades'
     def __str__(self):
         return f'{self.nome} - {self.Uf}'
-
 
 class Usuario(models.Model):
     nome= models.CharField(max_length=255)
@@ -73,13 +71,13 @@ class Livro(models.Model):
 
     def __str__(self):
         return f'{self.nome} - {self.genero}'
+    
 class Emprestimo(models.Model):
     usuario= models.ForeignKey(Usuario, on_delete=models.CASCADE)
     livros= models.ManyToManyField(Livro)
     data_emprestimo= models.DateField()
     data_devolucao= models.DateField(null=True, blank=True)
-
-
+    
     class Meta:
         verbose_name = 'Empréstimo'
         verbose_name_plural = 'Empréstimos'
